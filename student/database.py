1b553686c350948486fb5f88db576315ad736937
SRaige42 <krhaast24@gmail.com>
2024-11-28 00:20:02 +0800

    Update database URL to use postgresql dialect

--- student/database.py
+++ student/database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 DATABASE_URL = settings.DATABASE_URL
-engine = create_engine(DATABASE_URL)
+engine = create_engine(os.getenv("DATABASE_URL").replace("postgres://", "postgresql://"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
